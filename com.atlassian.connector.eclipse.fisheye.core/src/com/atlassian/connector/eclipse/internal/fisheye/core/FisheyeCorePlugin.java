2104423a1e28a5f60d6be5b90fb43c0e72616ef7
wseliga <devnull@localhost>
2009-04-08 23:14:38 +0000

    PLE-397 under dev

--- com.atlassian.connector.eclipse.fisheye.core/src/com/atlassian/connector/eclipse/internal/fisheye/core/FisheyeCorePlugin.java
+++ com.atlassian.connector.eclipse.fisheye.core/src/com/atlassian/connector/eclipse/internal/fisheye/core/FisheyeCorePlugin.java
@@ -1,49 +1,63 @@
-/**
- * 
- */
 package com.atlassian.connector.eclipse.internal.fisheye.core;
 
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
-import com.atlassian.connector.eclipse.internal.core.AtlassianLogger;
-import com.atlassian.theplugin.commons.util.LoggerImpl;
-
 /**
- * The activator class controls the plug-in life cycle.
- * 
- * @author Thomas Ehrnhoefer
- *
+ * The activator class controls the plug-in life cycle
  */
-public class FisheyeCorePlugin extends Plugin {
+public class FishEyeCorePlugin extends Plugin {
 
-	//plug-in ID
-	public static final String PLUGIN_ID = "The activator class controls the plug-in life cycle";
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.atlassian.connector.eclipse.fisheye.core";
 	
 	public static final String CONNECTOR_KIND = "fisheye";
 	
-	//shared instance
-	private static FisheyeCorePlugin plugin;
 
-	public FisheyeCorePlugin() {
-		// make sure that we have the logging going to the eclipse log
-		LoggerImpl.setInstance(new AtlassianLogger());
-	}
+	private FishEyeRepositoryConnector repositoryConnector;
 	
-	public static FisheyeCorePlugin getDefault() {
-		return plugin; 
-	}
+	// The shared instance
+	private static FishEyeCorePlugin plugin;
 	
-	@Override
+	/**
+	 * The constructor
+	 */
+	public FishEyeCorePlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
-	
-	@Override
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static FishEyeCorePlugin getDefault() {
+		return plugin;
+	}
+
+	public void setRepositoryConnector(FishEyeRepositoryConnector repositoryConnector) {
+		this.repositoryConnector = repositoryConnector;
+	}
+
+	public FishEyeRepositoryConnector getRepositoryConnector() {
+		return repositoryConnector;
 	}
 
 }
